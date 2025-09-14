12bedf75d4b92ee9142f56a2f67bbb9a3ca12e41
thinh <21135681.thinh@student.iuh.edu.vn>
2025-09-14 15:20:25 +0700

    Add template email for order-service

---
+++ src/main/java/iuh/fit/se/repository/httpclient/AuthClient.java
@@ -0,0 +1,10 @@
+//package iuh.fit.se.repository.httpclient;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@FeignClient(name = "auth-service", configuration = {})
+//public interface AuthClient {
+//    @GetMapping("/users/myinfo")
+//    UserResponse
+//}
